0951d6946d3ef27022f03e15905854d6a6732734
seunghyl <lutious2@gmail.com>
2020-07-09 22:01:36 +0900

    1

---
+++ C_Piscine_C_01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghyl <lutious2@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 23:20:42 by seunghyl          #+#    #+#             */
+/*   Updated: 2020/07/09 19:13:24 by seunghyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int		temp;
+	int		index;
+	int		rev_index;
+
+	index = 0;
+	rev_index = size - 1;
+	while (index < rev_index)
+	{
+		temp = *(tab + index);
+		*(tab + index) = *(tab + rev_index);
+		*(tab + rev_index) = temp;
+		index++;
+		rev_index--;
+	}
+}
