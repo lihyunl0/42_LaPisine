ae18bd2ae7ba6932397bfa02d0bf2a40cc66ac80
seunghyl <lutious2@gmail.com>
2020-07-14 09:20:36 +0900

    1

--- C_Piscine_Shell_01/ex04/MAC.sh
+++
@@ -1,1 +0,0 @@
-ifconfig | grep -w 'ether' | awk {'print $2'}
---
+++ ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghyl <lutious2@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 09:06:01 by seunghyl          #+#    #+#             */
+/*   Updated: 2020/07/14 09:19:35 by seunghyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
